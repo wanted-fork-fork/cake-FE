72d7ec535ad893442484cefa505218554dfab87e
ooooorobo <orobos654@gmail.com>
2021-10-31 20:06:03 +0900

    fix: get form value from target
    - useForm 훅을 만들기 전까지는 e.target.[name].value 값으로 폼의 인풋 값에 접근합니다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { FormEvent, useCallback } from "react";
 import styled from "styled-components";
 import { observer } from "mobx-react";
 
@@ -7,20 +8,21 @@ import { useStores } from "@src/store/root.store";
 
 // templates
 import LoginPageTemplate from "@src/templates/LoginPage.template";
-import { FormEvent, useCallback } from "react";
-import { LoginDto } from "@src/dto/auth.dto";
 
-const Container = styled.div`
-  //background-color: black;
-`;
+const Container = styled.div``;
 
 const Home: NextPage = observer(() => {
   const { userStore } = useStores();
 
-  const onSubmit = useCallback(async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await userStore.login({ ...e.target.value })
-  }, []);
+  const onSubmit = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const email = e.target.email.value;
+      const password = e.target.password.value;
+      await userStore.login({ email, password });
+    },
+    [userStore],
+  );
 
   return (
     <Container>
