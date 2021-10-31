339e7c51368126fd73f92884619f0b133fea5449
ooooorobo <orobos654@gmail.com>
2021-10-31 21:06:43 +0900

    fix: set default base url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    BASE_URL: process.env.BASE_URL,
+    BASE_URL: process.env.BASE_URL || "http://13.124.94.121:8080",
     IS_DEV: process.env.NODE_ENV === "development",
   },
 };
