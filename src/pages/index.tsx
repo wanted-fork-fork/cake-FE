3d0ac1f3e2451876dc62ab02decd3728653e9a0b
ooooorobo <orobos654@gmail.com>
2021-10-31 13:12:43 +0900

    feat: Add login form in root page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,29 @@ import type { NextPage } from "next";
 import styled from "styled-components";
 import { observer } from "mobx-react";
 
-// store
+// stores
 import { useStores } from "@src/store/root.store";
+
+// templates
+import LoginPageTemplate from "@src/templates/LoginPage.template";
+import { FormEvent, useCallback } from "react";
+import { LoginDto } from "@src/dto/auth.dto";
 
 const Container = styled.div`
   //background-color: black;
 `;
 
 const Home: NextPage = observer(() => {
-  const { countStore } = useStores();
+  const { userStore } = useStores();
+
+  const onSubmit = useCallback(async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await userStore.login({ ...e.target.value })
+  }, []);
+
   return (
     <Container>
-      <p>{countStore.number}</p>
-      <button onClick={countStore.increase} type="button">
-        +
-      </button>
-      <button onClick={countStore.decrease} type="button">
-        -
-      </button>
+      <LoginPageTemplate onSubmit={onSubmit} />
     </Container>
   );
 });
