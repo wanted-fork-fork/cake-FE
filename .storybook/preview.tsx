f6164fa7bd098f48addb2e30ce45768aedea8988
ooooorobo <orobos654@gmail.com>
2021-11-08 21:45:41 +0900

    style: import global fonts and add viewport to storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import theme from "@src/styles/theme";
 import GlobalStyle from "@src/styles/globals";
+import GlobalFonts from "@src/styles/fonts";
 import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 export const decorators = [
   Story => [
     <>
       <GlobalStyle />
+      <GlobalFonts/>
       <ThemeProvider theme={theme}>
         <Story />
       </ThemeProvider>
@@ -18,8 +20,17 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   layout: "fullscreen",
   viewport: {
+    defaultViewport: "iphoneX",
     viewports: {
       ...MINIMAL_VIEWPORTS,
+      iphoneX: {
+        name:"iPhone X",
+        styles: {
+          height:"812px",
+          width: "375px",
+        },
+        type: "mobile"
+      },
       mobile1: {
         name: "Small mobile",
         styles: {
