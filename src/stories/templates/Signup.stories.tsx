ca490cc316ca9595fd310793612528a5e6ffd6e1
ooooorobo <orobos654@gmail.com>
2021-11-09 19:48:05 +0900

    add dummy data in signup template story

--- src/stories/templates/Signup.stories.tsx
+++ src/stories/templates/Signup.stories.tsx
@@ -17,3 +17,67 @@ export const Signup = Template.bind({});
 Signup.args = {
   step: SignupStep.CONFIRM_EMAIL,
 };
+
+export const SelectSchoolStep = Template.bind({});
+SelectSchoolStep.args = {
+  step: SignupStep.SELECT_SCHOOL,
+};
+export const ConfirmEmailStep = Template.bind({});
+ConfirmEmailStep.args = {
+  step: SignupStep.CONFIRM_EMAIL,
+};
+export const PasswordInputStep = Template.bind({});
+PasswordInputStep.args = {
+  step: SignupStep.PASSWORD_INPUT,
+};
+export const DetailsInputStep = Template.bind({});
+DetailsInputStep.args = {
+  step: SignupStep.DETAILS_INPUT,
+};
+export const SelectCategoryStep = Template.bind({});
+SelectCategoryStep.args = {
+  step: SignupStep.SELECT_GIVE_CATEGORY,
+  categoryList: [
+    {
+      id: 2,
+      name: "악기 연주",
+      img: "https://fork-fork-cake.s3.ap-northeast-2.amazonaws.com/category/b79b6885f.jpg",
+    },
+    {
+      id: 3,
+      name: "주식투자",
+      img: "https://fork-fork-cake.s3.ap-northeast-2.amazonaws.com/category/156bee140.jpg",
+    },
+    {
+      id: 4,
+      name: "악기 연주",
+      img: "https://fork-fork-cake.s3.ap-northeast-2.amazonaws.com/category/b79b6885f.jpg",
+    },
+    {
+      id: 5,
+      name: "주식투자",
+      img: "https://fork-fork-cake.s3.ap-northeast-2.amazonaws.com/category/156bee140.jpg",
+    },
+    {
+      id: 6,
+      name: "악기 연주",
+      img: "https://fork-fork-cake.s3.ap-northeast-2.amazonaws.com/category/b79b6885f.jpg",
+    },
+    {
+      id: 7,
+      name: "주식투자",
+      img: "https://fork-fork-cake.s3.ap-northeast-2.amazonaws.com/category/156bee140.jpg",
+    },
+    {
+      id: 9,
+      name: "악기 연주",
+      img: "https://fork-fork-cake.s3.ap-northeast-2.amazonaws.com/category/b79b6885f.jpg",
+    },
+    {
+      id: 8,
+      name: "주식투자",
+      img: "https://fork-fork-cake.s3.ap-northeast-2.amazonaws.com/category/156bee140.jpg",
+    },
+  ],
+  selectedList: [8],
+};
