f396e3617e9aa717b2f9ad4819cea35ce561cf03
ooooorobo <orobos654@gmail.com>
2021-11-09 23:24:05 +0900

    feat: add antd config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withAntdLess = require("next-plugin-antd-less");
+
+const nextConfig = {
   reactStrictMode: true,
   env: {
     BASE_URL:
@@ -8,3 +11,12 @@ module.exports = {
     IS_DEV: process.env.NODE_ENV === "development",
   },
 };
+
+module.exports = withAntdLess({
+  // modifyVars: { "@primary-color": "#fc1150" },
+  lessVarsFilePath: "./src/styles/variables.less",
+  ...nextConfig,
+  webpack(config) {
+    return config;
+  },
+});
