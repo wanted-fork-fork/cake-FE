e572960904969e92e673483915fc51154083d9a3
ooooorobo <orobos654@gmail.com>
2021-11-10 21:23:54 +0900

    feat: add enum for univ category

---
+++ src/constant/enum.constant.ts
@@ -0,0 +1,30 @@
+export enum SignupStep {
+  SELECT_SCHOOL,
+  CONFIRM_EMAIL,
+  PASSWORD_INPUT,
+  DETAILS_INPUT,
+  SELECT_GIVE_CATEGORY,
+  SELECT_TAKE_CATEGORY,
+}
+
+export enum UnivCategory {
+  HUMANITY, // 인문대
+  SOCIAL, // 사회대
+  ENGINEERING, // 공과대
+  NATURE, // 자연대
+  BUSINESS, // 경영대
+  EDUCATION, // 사범대
+  ARTS, // 예술대
+  IT, // 정보통신대
+}
+
+export const UnivCategoryEnumToLabel = {
+  [UnivCategory.HUMANITY]: "인문대학",
+  [UnivCategory.SOCIAL]: "사회대학",
+  [UnivCategory.ENGINEERING]: "공과대학",
+  [UnivCategory.NATURE]: "자연대학",
+  [UnivCategory.BUSINESS]: "경영대학",
+  [UnivCategory.EDUCATION]: "사범대학",
+  [UnivCategory.ARTS]: "예술대학",
+  [UnivCategory.IT]: "정보통신대학",
+};
