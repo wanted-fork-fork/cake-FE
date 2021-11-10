24e33fb1cdce5a2e3799f5d58f3019257cb3c898
ooooorobo <orobos654@gmail.com>
2021-11-10 14:10:12 +0900

    feat: add signup store to root store

--- src/store/root.store.ts
+++ src/store/root.store.ts
@@ -5,18 +5,24 @@ import AuthService from "@src/services/Auth.service";
 
 import Axios from "@src/lib/axios";
 import { AxiosInstance } from "axios";
+import SignupStore from "@src/store/signup.store";
+import SignupService from "@src/services/Signup.service";
 
 export class RootStore {
   axiosInstance: AxiosInstance;
 
   userStore: UserStore;
 
+  signupStore: SignupStore;
+
   constructor() {
     this.axiosInstance = Axios.createAxiosInstance();
 
     const authService = new AuthService(this.axiosInstance);
+    const signupService = new SignupService(this.axiosInstance);
 
     this.userStore = new UserStore(this, authService);
+    this.signupStore = new SignupStore(this, signupService);
   }
 }
 
