56734d82040ded8421b4b0b39f7d897747ef5ddb
ooooorobo <orobos654@gmail.com>
2021-11-13 01:09:41 +0900

    feat: add nav icons

---
+++ src/components/icon/Home.icon.tsx
@@ -0,0 +1,27 @@
+function HomeIcon() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.12033 11.6168L13.9404 4.52616C15.0466 3.63683 16.6247 3.64448 17.7223 4.54448L26.3477 11.6173C27.0426 12.1871 27.4455 13.0384 27.4455 13.9371V25.5347C27.4455 27.1915 26.1023 28.5347 24.4455 28.5347H7C5.34315 28.5347 4 27.1915 4 25.5347V13.9549C4 13.0461 4.412 12.1862 5.12033 11.6168Z"
+        stroke="black"
+        strokeLinecap="round"
+      />
+      <rect
+        x="12.3569"
+        y="17.8566"
+        width="6.49973"
+        height="10.6781"
+        fill="#FA5D66"
+        stroke="black"
+      />
+    </svg>
+  );
+}
+
+export default HomeIcon;
