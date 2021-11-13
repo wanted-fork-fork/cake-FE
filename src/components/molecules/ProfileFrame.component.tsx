764a308de1000786950e78109aa5697535da67a5
ooooorobo <orobos654@gmail.com>
2021-11-13 17:04:28 +0900

    fix: change profile fram style props

--- src/components/molecules/ProfileFrame.component.tsx
+++ src/components/molecules/ProfileFrame.component.tsx
@@ -3,24 +3,55 @@ import UploadComponent from "@src/components/molecules/Upload.component";
 import styled from "styled-components";
 import theme from "@src/styles/theme";
 import { BaseProps, BaseStyleProps } from "@src/styles/common";
+import { Resource } from "@src/models/dto/api-response";
+
+interface ProfileFrameProps {
+  mb?: string;
+  size?: "large" | "small";
+  allowUpload?: boolean;
+  imgSrc?: string;
+  onUploadImage?: (e: Resource) => void;
+}
 
 const ProfileImageWrapper = styled.div<BaseProps>`
-  ${BaseStyleProps}
+  ${BaseStyleProps};
 
   position: relative;
   width: fit-content;
-  margin-left: auto;
-  margin-right: auto;
 `;
-const CircleImageFrame = styled.div`
-  width: 80px;
-  height: 80px;
-  border-radius: 50%;
+const CircleImageFrame = styled.div<ProfileFrameProps>`
+  border: 6px solid ${theme.color.primary};
 
   img {
     width: auto;
-    height: 80px;
+    height: ${({ size }) => (size === "large" ? "80px" : "32px")};
   }
+
+  svg {
+    width: auto;
+    height: ${({ size }) => (size === "large" ? "48px" : "18px")};
+  }
+
+  ${({ size }) => {
+    switch (size) {
+      case "large":
+        return `  
+        width: 80px;
+       height: 80px;
+        `;
+      case "small":
+        return `  
+        width: 32px;
+        height: 32px;
+        border-width: 3px;
+        `;
+      default:
+        return `  
+        width: 80px;
+        height: 80px;`;
+    }
+  }}
+  border-radius: 50%;
 
   display: flex;
   justify-content: center;
@@ -30,8 +61,6 @@ const CircleImageFrame = styled.div`
   position: relative;
 
   background-color: ${theme.color.gray0};
-
-  border: 6px solid ${theme.color.primary};
 `;
 
 const S = {
@@ -39,15 +68,24 @@ const S = {
   CircleImageFrame,
 };
 
+ProfileFrameComponent.defaultProps = {
+  mb: "",
+  size: "large",
+  allowUpload: true,
+  imgSrc: "",
+  onUploadImage: null,
+};
+
 function ProfileFrameComponent({
   mb = "",
-  allowUpload,
-  imgSrc,
-  onUploadImage,
-}) {
+  size = "large",
+  allowUpload = true,
+  imgSrc = "",
+  onUploadImage = null,
+}: ProfileFrameProps) {
   return (
     <S.ProfileImageWrapper mb={mb}>
-      <S.CircleImageFrame>
+      <S.CircleImageFrame size={size}>
         {imgSrc ? <img src={imgSrc} alt="프로필 이미지" /> : <PatisserieIcon />}
       </S.CircleImageFrame>
       {allowUpload && <UploadComponent onUploaded={onUploadImage} />}
