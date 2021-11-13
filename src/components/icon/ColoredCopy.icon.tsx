d36ec5d739487bccb39e334ce64bd243bd310585
ooooorobo <orobos654@gmail.com>
2021-11-13 17:03:09 +0900

    feat: add icons

---
+++ src/components/icon/ColoredCopy.icon.tsx
@@ -0,0 +1,33 @@
+function ColoredCopyIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="5.5"
+        y="4.5"
+        width="10"
+        height="12"
+        fill="white"
+        stroke="#FC1150"
+      />
+      <rect
+        x="9"
+        y="7.5"
+        width="9.5"
+        height="12.25"
+        fill="white"
+        stroke="#FC1150"
+      />
+      <path d="M10.8335 10.3125H16.6668" stroke="#FC1150" />
+      <path d="M10.8335 13.625H16.6668" stroke="#FC1150" />
+      <path d="M10.8335 16.9375H16.6668" stroke="#FC1150" />
+    </svg>
+  );
+}
+
+export default ColoredCopyIcon;
