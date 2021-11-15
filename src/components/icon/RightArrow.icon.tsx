2c1a7740e760d2102e7c7cdc0e599c64fc666f57
ooooorobo <orobos654@gmail.com>
2021-11-15 21:48:15 +0900

    feat: add new icons

---
+++ src/components/icon/RightArrow.icon.tsx
@@ -0,0 +1,15 @@
+function RightArrowIcon() {
+  return (
+    <svg
+      width="36"
+      height="36"
+      viewBox="0 0 36 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M15 25L22.5 17.5L15 10" stroke="black" strokeWidth="2" />
+    </svg>
+  );
+}
+
+export default RightArrowIcon;
