4c48821eea34e3e0de1516d1cfc4f633207fdc9e
ooooorobo <orobos654@gmail.com>
2021-11-30 22:51:49 +0900

    chore: update package deps

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -13,6 +13,7 @@ import "cypress-localstorage-commands";
 
 // -- This is a parent command --
 export const dummyToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c`;
+export default {};
 Cypress.Commands.add("login", () => {
   cy.setLocalStorage("accessToken", dummyToken);
 });
