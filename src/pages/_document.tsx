e705c61e28a60c9ab6f0b48888d96cc7a816e995
ooooorobo <orobos654@gmail.com>
2021-12-02 19:48:28 +0900

    fix: move icon to icon dir

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,71 +43,71 @@ export default class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="57x57"
-            href="/apple-icon-57x57.png"
+            href="/icon/apple-icon-57x57.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="60x60"
-            href="/apple-icon-60x60.png"
+            href="/icon/apple-icon-60x60.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="72x72"
-            href="/apple-icon-72x72.png"
+            href="/icon/apple-icon-72x72.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/apple-icon-76x76.png"
+            href="/icon/apple-icon-76x76.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="114x114"
-            href="/apple-icon-114x114.png"
+            href="/icon/apple-icon-114x114.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="120x120"
-            href="/apple-icon-120x120.png"
+            href="/icon/apple-icon-120x120.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="144x144"
-            href="/apple-icon-144x144.png"
+            href="/icon/apple-icon-144x144.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="152x152"
-            href="/apple-icon-152x152.png"
+            href="/icon/apple-icon-152x152.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-icon-180x180.png"
+            href="/icon/apple-icon-180x180.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
-            href="/android-icon-192x192.png"
+            href="/icon/android-icon-192x192.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="/icon/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="96x96"
-            href="/favicon-96x96.png"
+            href="/icon/favicon-96x96.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/icon/favicon-16x16.png"
           />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#fc1150" />
